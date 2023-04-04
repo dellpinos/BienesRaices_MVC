06830729f5e45fc1e917b8e562c70a1ca0f43d9e
Martin del Pino <dellpinos7@gmail.com>
2023-04-04 14:20:32 -0300

    Comenzando con el Router

--- Router.php
+++ Router.php
@@ -4,4 +4,26 @@ namespace MVC;
 
 class Router {
 
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+
+    public function get($url, $fn){
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas(){
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if($metodo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? null; // Función asociada a esta url
+        }
+        if($fn){
+            // Hay una funcion asociada a la url
+            call_user_func($fn, $this);
+        } else {
+            echo "#404 Pagina inexistente   :(";
+        }
+    }
 }
--- public/index.php
+++ public/index.php
@@ -5,3 +5,10 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 
 $router = new Router();
+
+$router->get('/', 'funcion_nosotros');
+$router->get('/anuncio', 'funcion_anuncio');
+$router->get('/anuncios', 'funcion_anuncios');
+$router->get('/contacto', 'funcion_contacto');
+
+$router->comprobarRutas();
